b7167586ee66ab2cd7b61d636642eec3eb2f76cc
JaydenSoong <jiandongsong@126.com>
2017-11-27 17:04:55 +0800

    注册与登录案例

---
+++ register_login/src/com/jayden/user/exception/UserException.java
@@ -0,0 +1,26 @@
+package com.jayden.user.exception;
+
+/**
+ * 自定义异常类，用于在用户名已经被注册过时返回信息
+ */
+public class UserException extends Exception{
+    public UserException() {
+        super();
+    }
+
+    public UserException(String message) {
+        super(message);
+    }
+
+    public UserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserException(Throwable cause) {
+        super(cause);
+    }
+
+    protected UserException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
